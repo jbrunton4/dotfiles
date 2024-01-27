707c80a521064c4cbc863ddfd0cb18471a02a9de
Josh Brunton <bruntonjoshua9@gmail.com>
2024-01-27 22:31:34 +0000

    Create file backups (#4)

---
+++ setup/before/backup.sh
@@ -0,0 +1,8 @@
+backup_filepath="$HOME/.dotfiles-backup/$(date +%s%N)"
+mkdir $backup_filepath
+
+cp -r "$HOME/.config/nvim" "$backup_filepath/nvim"
+cp -r "$HOME/.config/neofetch" "$backup_filepath/neofetch" 
+
+cp -r "$HOME/.tmux.conf" "$backup_filepath/.tmux.conf"
+cp -r "$HOME/.bashrc" "$backup_filepath/.bashrc"
