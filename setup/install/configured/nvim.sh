52a7a64123d584cd942f8b67d4a100457dff0027
Josh Brunton <39703189+jbrunton4@users.noreply.github.com>
2024-01-28 14:32:42 +0000

    Fix a bug that caused nvim installation to freeze

--- setup/install/configured/nvim.sh
+++ setup/install/configured/nvim.sh
@@ -1,7 +1,7 @@
 yes | add-apt-repository ppa:neovim-ppa/unstable
 yes | apt-get install neovim
 
-rm -rf $HOME/.config/nvim/* 
+rm -rf $HOME/.config/nvim 
 clone_folder_name="./nvim-$(date +%s%N)"
 git clone https://github.com/jbrunton4/dotfiles.git --branch main --single-branch --depth 1 $clone_folder_name
 mkdir $HOME/.config/nvim
