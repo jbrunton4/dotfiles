80dde900abde938d480b168c4c192318e98e3d13
Josh Brunton <bruntonjoshua9@gmail.com>
2024-01-28 13:35:49 +0000

    Automatically clone neovim config (#9)

--- setup/install/configured/nvim.sh
+++ setup/install/configured/nvim.sh
@@ -1,5 +1,12 @@
 yes | add-apt-repository ppa:neovim-ppa/unstable
 yes | apt-get install neovim
 
-nvim --cmd "source $HOME/.config/nvim/init.lua" --cmd "qall"
-nvim --cmd "PackerSync" --cmd "qall"
+rm -rf $HOME/.config/nvim/* 
+clone_folder_name="./nvim-$(date +%s%N)"
+git clone https://github.com/jbrunton4/dotfiles.git --branch main --single-branch --depth 1 $clone_folder_name
+mkdir $HOME/.config/nvim
+mv $clone_folder_name/nvim/* $HOME/.config/nvim
+rm -rf $clone_folder_name
+
+nvim --cmd "so $HOME/.config/nvim/init.lua" --cmd "qall"
+nvim --headless -c "lua require('packer').sync()" -c "qa"
