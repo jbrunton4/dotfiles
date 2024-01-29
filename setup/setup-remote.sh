5e50baf7c066b49004eb266bcae2e3ca53efb486
Josh Brunton <bruntonjoshua9@gmail.com>
2024-01-29 20:25:21 +0000

    Sudo bash on main script (#17)

--- setup/setup-remote.sh
+++ setup/setup-remote.sh
@@ -2,6 +2,6 @@ initial_dir="$(pwd)"
 repo_dir="dotfiles-$(date +%s)"
 git clone "https://github.com/jbrunton4/dotfiles" "${repo_dir}"
 cd $repo_dir/setup
-/bin/bash ./setup.sh
+sudo /bin/bash ./setup.sh
 cd $initial_dir
 rm -rf $repo_dir
