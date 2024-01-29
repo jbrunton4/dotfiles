abb43b9a5574fc9df6ae48ef2cd0060abf372866
Josh Brunton <bruntonjoshua9@gmail.com>
2024-01-29 22:56:12 +0000

    Auto install packer.nvim

--- setup/install/configured/nvim.sh
+++ setup/install/configured/nvim.sh
@@ -1,6 +1,8 @@
 yes | add-apt-repository ppa:neovim-ppa/unstable
 yes | apt-get install neovim
 
+git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 rm -rf $HOME/.config/nvim 
 clone_folder_name="./nvim-$(date +%s%N)"
 git clone https://github.com/jbrunton4/dotfiles.git --branch main --single-branch --depth 1 $clone_folder_name
@@ -8,5 +10,5 @@ mkdir $HOME/.config/nvim
 mv $clone_folder_name/nvim/* $HOME/.config/nvim
 rm -rf $clone_folder_name
 
-nvim --cmd "so $HOME/.config/nvim/init.lua" --cmd
-nvim --headless -c "lua require('packer').sync()" -c "qa"
+nvim --cmd "so $HOME/.config/nvim/init.lua" -c "qa"
+nvim --cmd "lua require('packer').sync()" -c "qa"
