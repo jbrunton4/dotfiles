8d5a6b202d7197a0ffb663519ec8a2dea40618fe
Josh Brunton <39703189+jbrunton4@users.noreply.github.com>
2024-01-30 09:14:24 +0000

    Create a dotfiles-refresh command to further automate subsequent installs

---
+++ setup/install/simple/dotfiles_refresh.sh
@@ -0,0 +1,3 @@
+refresh_script_path="/usr/bin/dotfiles-refresh"
+curl -sSL https://raw.githubusercontent.com/jbrunton4/dotfiles/main/setup/setup-remote.sh > $refresh_script_path
+chmod +x $refresh_script_path
