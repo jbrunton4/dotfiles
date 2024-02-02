6492b4717af5a4860a81d2195ed203f885b01a62
Josh <josh.brunton@proton.me>
2024-02-02 11:32:19 +0000

    Compile newsboat URLs from JSON file (#27, #35)

--- setup/install/configured/newsboat.sh
+++ setup/install/configured/newsboat.sh
@@ -1,6 +1,18 @@
+
 snap install newsboat 
 
 newsboat_dir="$HOME/snap/newsboat/7339/.newsboat"
 mkdir -p $newsboat_dir
 touch $newsboat_dir/urls
-curl https://raw.githubusercontent.com/jbrunton4/dotfiles/main/newsboat/urls > $newsboat_dir/urls
+
+curl https://raw.githubusercontent.com/jbrunton4/dotfiles/main/newsboat/urls.json > ./urls.temp.json
+
+echo "" > $newsboat_dir/urls
+output=$(jq -r 'recurse(.[]?) | scalars' ./urls.temp.json)
+for ((i=0; i<${#output[@]}; i++)); do    
+    item="${output[i]}"
+    echo -e "${item}\n" >> $newsboat_dir/urls
+    echo -e "${item}\n"
+done
+
+rm -f ./urls.temp.json
