f491109c64a99783885419a171fd51539a86cd21
Josh Brunton <bruntonjoshua9@gmail.com>
2024-02-03 23:26:56 +0000

    Use git to track backups (#25)

--- setup/before/backup.sh
+++ setup/before/backup.sh
@@ -1,12 +1,21 @@
-backup_filepath="$HOME/.brunt-dotfiles/backup/$(date +%s%N)"
-mkdir -p $backup_filepath
+backup_filepath="$HOME/.brunt-dotfiles/backup"
+mkdir -p "$backup_filepath"
+
+if [ -d "$backup_filepath/.git" ]
+then
+    : # pass
+else
+    git init "$backup_filepath"
+fi
+
+find "$backup_filepath" -mindepth 1 ! -path "$backup_filepath/.git*" -delete
 
 cp -r "$HOME/.config/nvim" "$backup_filepath/nvim"
 cp -r "$HOME/.config/neofetch" "$backup_filepath/neofetch" 
-
 cp -r "$HOME/.tmux.conf" "$backup_filepath/.tmux.conf"
 cp -r "$HOME/.bashrc" "$backup_filepath/.bashrc"
-
 cp -r "$HOME/.config/newsboat/urls" "$backup_filepath/newsboat/urls"
 
-cp -r -f $backup_filepath "$HOME/.brunt-dotfiles/backup/latest"
+git -C "$backup_filepath" add --all
+git -C "$backup_filepath" commit -m "$(date +%s%N)"
+git -C "$backup_filepath" gc --aggressive
