d0cff12191c92924777217985949d02838ff590a
Josh Brunton <bruntonjoshua9@gmail.com>
2024-02-03 15:45:11 +0000

    Bugfix: Line endings in newsboat install script caused filesystem issues

--- setup/install/configured/newsboat.sh
+++ setup/install/configured/newsboat.sh
@@ -5,14 +5,15 @@ newsboat_dir="$HOME/snap/newsboat/7339/.newsboat"
 mkdir -p $newsboat_dir
 touch $newsboat_dir/urls
 
-curl https://raw.githubusercontent.com/jbrunton4/dotfiles/main/newsboat/urls.json > ./urls.temp.json
+touch "./urls.temp.json"
+curl https://raw.githubusercontent.com/jbrunton4/dotfiles/main/newsboat/urls.json > "./urls.temp.json"
 
 echo "" > $newsboat_dir/urls
-output=$(jq -r 'recurse(.[]?) | scalars' ./urls.temp.json)
+output=($(jq -r 'recurse(.[]?) | scalars' ./urls.temp.json))
 for ((i=0; i<${#output[@]}; i++)); do    
     item="${output[i]}"
     echo -e "${item}\n" >> $newsboat_dir/urls
     echo -e "${item}\n"
 done
 
-rm -f ./urls.temp.json
+rm -f "./urls.temp.json"
