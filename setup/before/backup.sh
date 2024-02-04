1ef1d48285efe2bc51c4ef528f289e236ed529b2
Josh Brunton <bruntonjoshua9@gmail.com>
2024-02-04 18:22:21 +0000

    Clean up backup folder file structure (#44)

--- setup/before/backup.sh
+++ setup/before/backup.sh
@@ -6,17 +6,20 @@ then
     : # pass
 else
     git init "$backup_filepath"
+    git -C "$backup_filepath" add --all
+    git -C "$backup_filepath" commit -m "$(date +%s%N)"
 fi
 
 find "$backup_filepath" -mindepth 1 ! -path "$backup_filepath/.git*" -delete
 
 cp -r "$HOME/.config/nvim" "$backup_filepath/nvim"
 cp -r "$HOME/.config/neofetch" "$backup_filepath/neofetch" 
-cp -r "$HOME/.tmux.conf" "$backup_filepath/.tmux.conf"
-cp -r "$HOME/.bashrc" "$backup_filepath/.bashrc"
+cp -r "$HOME/.tmux.conf" "$backup_filepath/tmux/.tmux.conf"
+cp -r "$HOME/.bashrc" "$backup_filepath/bash/.bashrc"
+cp -r "$HOME/.bash_aliases" "$backup_filepath/bash/.bash_aliases"
 cp -r "$HOME/.config/newsboat/urls" "$backup_filepath/newsboat/urls"
-cp -r "$HOME/.gitconfig" "$backup_filepath/.gitconfig"
+cp -r "$HOME/git/.gitconfig" "$backup_filepath/git/.gitconfig"
 
 git -C "$backup_filepath" add --all
 git -C "$backup_filepath" commit -m "$(date +%s%N)"
-git -C "$backup_filepath" gc --aggressive
+git -C "$backup_filepath" gc
