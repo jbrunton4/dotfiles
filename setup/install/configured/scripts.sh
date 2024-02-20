9f8ea54fb29b13a1f86342fd997a0155d2b9f6b2
Josh Brunton <bruntonjoshua9@gmail.com>
2024-02-20 23:27:43 +0000

    Assorted bugfixes

--- setup/install/configured/scripts.sh
+++ setup/install/configured/scripts.sh
@@ -4,14 +4,17 @@ config_dir="$HOME/.brunt-dotfiles/config"
 mkdir -p "$scripts_dir"
 mkdir -p "$config_dir"
 
-cp -r ../../../scripts/* "${scripts_dir}"
+curl -sSL https://raw.githubusercontent.com/jbrunton4/dotfiles/main/scripts/git-back.sh > "${scripts_dir}/git-back.sh"
+curl -sSL https://raw.githubusercontent.com/jbrunton4/dotfiles/main/scripts/open_visual_studio_pro_22.sh > "${scripts_dir}/open_visual_studio_pro_22.sh"
+curl -sSL https://raw.githubusercontent.com/jbrunton4/dotfiles/main/scripts/open_work_project.sh > "${scripts_dir}/open_work_project.sh"
 
 config_files=(
     "projects_directory"
+    "visual_studio_install_location"
 )
 for file in "${config_files[@]}"
 do
-    [ -f "${config_dir}/${file}" ] && touch "${config_dir}/${file}"
+    [ ! -f "${config_dir}/${file}" ] && touch "${config_dir}/${file}"
 done
 
 chmod +x "$scripts_dir/*"
