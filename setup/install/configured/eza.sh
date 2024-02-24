83c2e0f236c7e8d89473a59d65b2fda13a5ddd9e
Josh Brunton <bruntonjoshua9@gmail.com>
2024-02-24 22:20:20 +0000

    Use yes in eza install

--- setup/install/configured/eza.sh
+++ setup/install/configured/eza.sh
@@ -1,6 +1,6 @@
-sudo apt install gpg
+sudo apt install -y gpg
 mkdir -p /etc/apt/keyrings
 yes | wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | tee /etc/apt/sources.list.d/gierens.list
-chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-sudo apt install eza
+yes | echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | tee /etc/apt/sources.list.d/gierens.list
+yes | chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+sudo apt install -y eza
