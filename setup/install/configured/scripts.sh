8acd4053acba50be870ea4216894b14f8b4674f4
Josh Brunton <bruntonjoshua9@gmail.com>
2024-02-24 19:32:29 +0000

    Code cleanup for scripts install

--- setup/install/configured/scripts.sh
+++ setup/install/configured/scripts.sh
@@ -6,13 +6,18 @@ config_dir="$HOME/.brunt-dotfiles/config"
 mkdir -p "$scripts_dir"
 mkdir -p "$config_dir"
 
-curl -sSL https://raw.githubusercontent.com/jbrunton4/dotfiles/main/scripts/open_visual_studio_pro_22.sh > "${scripts_dir}/open_visual_studio_pro_22.sh"
-curl -sSL https://raw.githubusercontent.com/jbrunton4/dotfiles/main/scripts/open_work_project.sh > "${scripts_dir}/open_work_project.sh"
-
+scripts=(
+    "open_visual_studio_pro_22",
+    "open_work_project",
+)
 if [ $(jq '.profile' $HOME/.brunt-dotfiles/config/install.json) -eq "home" ]; then
-    curl -sSL https://raw.githubusercontent.com/jbrunton4/dotfiles/main/scripts/lolcat_block.sh > "${scripts_dir}/lolcat_block.sh"
-
+    scripts+=("lolcat_block")
 fi
+
+for script_name in "${scripts[@]}"
+do
+    curl -sSL https://raw.githubusercontent.com/jbrunton4/dotfiles/main/scripts/${script_name}.sh > "${scripts_dir}/${script_name}.sh"
+done
 
 config_files=(
     "projects_directory"
