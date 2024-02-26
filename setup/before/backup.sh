39b84a7104175626bc0e3c1159cedb45dfaa3373
Josh Brunton <bruntonjoshua9@gmail.com>
2024-02-26 22:08:00 +0000

    Back up some more files

--- setup/before/backup.sh
+++ setup/before/backup.sh
@@ -14,13 +14,17 @@ fi
 
 find "$backup_filepath" -mindepth 1 ! -path "$backup_filepath/.git*" -delete
 
+cp -r "$HOME/.brunt-dotfiles" "$backup_filepath/.brunt-dotfiles"
+
 cp -r "$HOME/.config/nvim" "$backup_filepath/nvim"
 cp -r "$HOME/.config/neofetch" "$backup_filepath/neofetch" 
+cp -r "$HOME/.config/newsboat/urls" "$backup_filepath/newsboat/urls"
 cp -r "$HOME/.tmux.conf" "$backup_filepath/tmux/.tmux.conf"
+cp -r "$HOME/git/.gitconfig" "$backup_filepath/git/.gitconfig"
+cp -r "/etc/wsl.conf" "$backup_filepath/etc/wsl.conf"
+cp -r "/etc/resolv.conf" "$backup_filepath/etc/resolv.conf"
 cp -r "$HOME/.bashrc" "$backup_filepath/bash/.bashrc"
 cp -r "$HOME/.bash_aliases" "$backup_filepath/bash/.bash_aliases"
-cp -r "$HOME/.config/newsboat/urls" "$backup_filepath/newsboat/urls"
-cp -r "$HOME/git/.gitconfig" "$backup_filepath/git/.gitconfig"
 
 git -C "$backup_filepath" add --all
 git -C "$backup_filepath" commit -m "$(date +%s%N)"
