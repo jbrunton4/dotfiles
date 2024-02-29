f395052a87b88808503b88d2af524e42ee9985e5
Josh Brunton <bruntonjoshua9@gmail.com>
2024-02-29 22:12:56 +0000

    Use bootandy/dust

---
+++ setup/install/simple/cargo_crates.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# install sscache first, to skip re-compiling shared dependencies
+echo "===== CARGO INSTALL: $package =====" >> $HOME/.brunt-dotfiles/logs/latest
+cargo install sccache >> $HOME/.brunt-dotfiles/logs/latest
+
+packages=(
+    "du-dust"
+)
+
+for package in "${packages[@]}"
+do
+    echo "===== CARGO INSTALL: $package =====" >> $HOME/.brunt-dotfiles/logs/latest
+    cargo install $package >> $HOME/.brunt-dotfiles/logs/latest
+done
