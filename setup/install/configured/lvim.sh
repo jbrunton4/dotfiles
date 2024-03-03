1acfc5b6c7cef4f14df53e43dbe6330340604f1f
Josh Brunton <bruntonjoshua9@gmail.com>
2024-03-03 19:23:43 +0000

    Auto isntall lvim config

--- setup/install/configured/lvim.sh
+++ setup/install/configured/lvim.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+
+mkdir -p $HOME/.config/lvim
+curl -sSL https://github.com/jbrunton4/dotfiles/main/userhome/.config/lvim/config.lua > $HOME/.config/lvim/config.lua
