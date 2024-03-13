c990dcea7efdde6963add845e6b8cd3af127d249
Josh Brunton <bruntonjoshua9@gmail.com>
2024-03-13 08:23:17 +0000

    Git configs based on profile

--- setup/install/configured/gitconfig.sh
+++ setup/install/configured/gitconfig.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 curl -sSL "https://raw.githubusercontent.com/jbrunton4/dotfiles/main/userhome/.gitconfig" > "$HOME/.gitconfig"
+
+if [ "$(jq '.profile' $HOME/.brunt-dotfiles/config/install.json)" == "\"home\"" ]; then
+    git config --global user.email josh.brunton@proton.me
+    git config --global github.user jbrunton4
+elif [ "$(jq '.profile' $HOME/.brunt-dotfiles/config/install.json)" == "\"work\"" ]; then
+    git config --global user.email josh.brunton@binary.ax
+fi
