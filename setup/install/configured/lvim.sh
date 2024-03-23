2daff98263c7ce70f0ba581d82015f6e60a26fe5
Josh Brunton <josh.brunton@proton.me>
2024-03-23 17:43:33 +0000

    Add neovim install before lvim

--- setup/install/configured/lvim.sh
+++ setup/install/configured/lvim.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+apt install neovim 
+
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
 mkdir -p $HOME/.config/lvim
