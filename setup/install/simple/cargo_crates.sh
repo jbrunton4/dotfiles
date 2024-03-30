b835d7bf72af80dfc9c2982cac674057782c2b3f
Josh Brunton <root@DESKTOP-D7BUSPN>
2024-03-30 23:27:01 +0000

    Explicitly install openssl before sccache

--- setup/install/simple/cargo_crates.sh
+++ setup/install/simple/cargo_crates.sh
@@ -2,6 +2,8 @@
 
 # install sscache first, to skip re-compiling shared dependencies
 echo "===== CARGO INSTALL: $package =====" >> $HOME/.brunt-dotfiles/logs/latest
+
+apt install openssl >> $HOME/.brunt-dotfiles/logs/latest
 cargo install sccache >> $HOME/.brunt-dotfiles/logs/latest
 
 packages=(
