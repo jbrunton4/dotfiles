8f71b0bb99fd390c0aff95c3e11343ec1ed846de
Josh Brunton <bruntonjoshua9@gmail.com>
2024-03-30 21:18:12 +0000

    Fix URL for lvim config.lua

--- setup/install/configured/lvim.sh
+++ setup/install/configured/lvim.sh
@@ -6,4 +6,4 @@ apt install neovim
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
 mkdir -p $HOME/.config/lvim
-curl -sSL https://github.com/jbrunton4/dotfiles/main/userhome/.config/lvim/config.lua > $HOME/.config/lvim/config.lua
+curl -sSL https://raw.githubusercontent.com/jbrunton4/dotfiles/main/userhome/.config/lvim/config.lua > $HOME/.config/lvim/config.lua
