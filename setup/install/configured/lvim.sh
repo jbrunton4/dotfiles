d4d64e8b8110c8b5d786a890036616dfb3211a66
Josh Brunton <bruntonjoshua9@gmail.com>
2024-03-30 21:13:44 +0000

    Add unstable nvim repo to allow lvim to install first time

--- setup/install/configured/lvim.sh
+++ setup/install/configured/lvim.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+add-apt-repository ppa:neovim-ppa/unstable
 apt install neovim 
 
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
