f3f804ccd35802c05b22d88e13a30695b6dfd758
Josh Brunton <bruntonjoshua9@gmail.com>
2024-04-14 11:53:20 +0100

    Home directory files

---
+++ userhome/.config/lvim/config.lua
@@ -0,0 +1,60 @@
+-- Indentation 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Wrapping
+vim.opt.wrap = true
+vim.opt.smartindent = true
+
+-- Scrolloff
+vim.scrolloff = 8
+
+-- Use relative line numbers
+vim.cmd.set "nu rnu"
+vim.cmd.set "autochdir"
+
+
+-- Keymaps
+-- move selected lines in visual mode by shift+dir
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- alt-arrow for moving windows 
+vim.keymap.set("n", "<M-Left>", ":wincmd h<CR>")
+vim.keymap.set("n", "<M-Down>", ":wincmd j<CR>")
+vim.keymap.set("n", "<M-Up>", ":wincmd k<CR>")
+vim.keymap.set("n", "<M-Right>", ":wincmd l<CR>")
+
+-- Default behaviour fixes
+-- paste over doesn't overwrite paste buffer
+vim.keymap.set("x", "<leader>p", "\"_dp")
+-- in half-page jump, cursor remains stationary
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+
+-- Lazy 
+lvim.plugins = {
+    {
+        "catppuccin/nvim",
+        config = function()
+            vim.cmd("colorscheme catppuccin-frappe")
+        end
+    },
+    { "lewis6991/gitsigns.nvim" },
+    { "andweeb/presence.nvim" },
+    { "tpope/vim-fugitive" },
+    { 
+        "christoomey/vim-tmux-navigator",
+        vim.keymap.set("n", "<M-Left>", ":TmuxNavigateLeft<CR>"),
+        vim.keymap.set("n", "<M-Down>", ":TmuxNavigateDown<CR>"),
+        vim.keymap.set("n", "<M-Up>", ":TmuxNavigateUp<CR>"),
+        vim.keymap.set("n", "<M-Right>", ":TmuxNavigateRight<CR>")
+    }
+}
+
+-- Package setup
+require('gitsigns').setup()
+require("presence").setup()
