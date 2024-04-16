7abcd4e092f5f0433f8c43c44a6fbc20a488be1a
Josh Brunton <josh.brunton@proton.me>
2024-04-16 08:05:08 +0100

    Add nvim signature snippet

--- setup/install/configured/lvim.sh
+++ setup/install/configured/lvim.sh
@@ -6,3 +6,5 @@ LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.
 
 mkdir -p $HOME/.config/lvim
 curl -sSL https://raw.githubusercontent.com/jbrunton4/dotfiles/main/userhome/.config/lvim/config.lua > $HOME/.config/lvim/config.lua
+mkdir -p $HOME/.config/lvim/luasnippets
+curl -sSL https://raw.githubusercontent.com/jbrunton4/dotfiles/main/userhome/.config/lvim/luasnippets/all.lua > $HOME/.config/lvim/luasnippets/all.lua
