35412ef6ee3a6e97d6d0d4a10c833669d74ff79a
Josh Brunton <josh.brunton@proton.me>
2024-04-22 23:00:19 +0100

    Git back drop

--- scripts/git-back.sh
+++ scripts/git-back.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+if [[ "$1" == "drop" ]]; then 
+    git branch --list 'backup/*' | xargs git branch -D
+    exit 0
+fi
+
 git stash -u -m "Backup at $(date): $(git diff --start | awk 'END{print}')" && git branch "backup/$(git rev-parse --abbrev-ref HEAD)/$(date +%s)" stash@{0} && git stash pop
