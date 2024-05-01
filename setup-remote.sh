b0bb61fb0d7ff9ab30fa13941591ea0d35be38ce
Josh Brunton <josh.brunton@proton.me>
2024-05-01 21:31:02 +0100

    Quick and dirty nvim config install

--- setup-remote.sh
+++ setup-remote.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-dpkg --configure -a 
+dpkg --configure -a
 
-apt update 
+apt update
 apt upgrade
-apt install -y git libssl-dev curl build-essential pkg-config 
+apt install -y git libssl-dev curl build-essential pkg-config
 
 if [ ! -f $HOME/.cargo/bin/cargo ]; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
-if cargo install --list | -grep -q "sccache"; then 
-    echo "Crate 'sccache' is already installed, skipping..."
+if cargo install --list | -grep -q "sccache"; then
+	echo "Crate 'sccache' is already installed, skipping..."
 else
-    $HOME/.cargo/bin/cargo install sccache
+	$HOME/.cargo/bin/cargo install sccache
 fi
 
 repo_dir="dotfiles-$(date +%s)"
 git clone "https://github.com/jbrunton4/dotfiles" "${repo_dir}"
+
+rm -r $HOME/.config/nvim
+cp -r "${repo_dir}/userhome/.config/nvim" $HOME/.config/nvim
 
 cargo run --jobs 1 --manifest-path "$repo_dir/installer/Cargo.toml"
 
