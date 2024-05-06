c79af552bc489e39b2054e1ba6abfe9eac979686
Josh Brunton <josh.brunton@proton.me>
2024-05-06 13:25:20 +0100

    Git-back drop

--- scripts/git-back.sh
+++ scripts/git-back.sh
@@ -1,8 +1,59 @@
 #!/bin/bash
 
-if [[ "$1" == "drop" ]]; then 
-    git branch --list 'backup/*' | xargs git branch -D
-    exit 0
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Check for git repo
+if [ -d ".git" ] || git rev-parse --git-dir >/dev/null 2>&1; then
+	: # pass
+else
+	echo -e "${RED}FATAL:${NC} Not a git repository"
+	exit 1
 fi
 
-git stash -u -m "Backup at $(date): $(git diff --start | awk 'END{print}')" && git branch "backup/$(git rev-parse --abbrev-ref HEAD)/$(date +%s)" stash@{0} && git stash pop
+# Check for branch head
+if git symbolic-ref -q HEAD >/dev/null 2>&1; then
+	: # pass
+else
+	echo -e "${RED}FATAL:${NC} Not a branch head"
+	exit 1
+fi
+
+git_backup() {
+	# Store info about the current state
+	old_branch=$(git rev-parse --abbrev-ref HEAD)
+	latest_commit_message=$(git log -1 --pretty=%B)
+	latest_commit_hash=$(git rev-parse HEAD)
+
+	# Compute info about the new state
+	new_branch="backup/${old_branch}/$(date +%s%N)"
+	new_commit_message="Backup atop ${latest_commit_hash} at $(date)"
+
+	# Create the backup
+	git reset
+	git checkout -b $new_branch
+	git add .
+	git commit -m "$new_commit_message"
+	new_commit_hash=$(git rev-parse HEAD)
+
+	# Revert working state to pre-backup state
+	git checkout $old_branch
+	git cherry-pick -n $new_branch
+	git reset
+}
+
+git_backup_delete_all() {
+	git branch | rg "^\s+backup/" | xargs git branch -D
+}
+
+if [[ "$@" == *"--drop"* ]]; then
+	git_backup_delete_all
+	exit 0
+fi
+
+git_backup >>/dev/null 2>&1
+echo -e "${GREEN}Success${NC}"
+echo "Created backup of working state"
+echo "  at ${new_commit_hash:0:7} in ${new_branch}"
+echo "  atop ${latest_commit_hash:0:7} on ${old_branch}"
